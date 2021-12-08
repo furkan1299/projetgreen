2052e2d22f12515096ded28cd35152af51539cce
furkan1299 <f.baran@students.ephec.be>
2021-12-08 15:07:40 +0100

    Firebase code de liaison

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase : {
+    apiKey: "AIzaSyBR9-pUccaB1ptwZ1Z-5EEf1hqWq0p1WGc",
+    authDomain: "les-recycleurs.firebaseapp.com",
+    projectId: "les-recycleurs",
+    storageBucket: "les-recycleurs.appspot.com",
+    messagingSenderId: "778078526302",
+    appId: "1:778078526302:web:807676ffa322cb2ad08dcf",
+    measurementId: "G-4RDZQJPWXH"
+    }
 };
