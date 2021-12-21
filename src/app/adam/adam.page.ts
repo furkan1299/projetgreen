d2fb68d4f276a5bb377f7a6744fcc01f34a790a7
furkan1299 <f.baran@students.ephec.be>
2021-12-21 23:08:18 +0100

    Baran Furkan_Design
    Design niveau CSS des différentes page et arrangement de la taille des boutons

--- src/app/adam/adam.page.ts
+++ src/app/adam/adam.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-adam',
@@ -11,12 +12,37 @@ export class AdamPage {
 
   constructor(
     private auth: AuthService,
-    private router: Router
+    private router: Router,
+    public alertController: AlertController
   ) { }
 
-    logout()
+  
+
+    async logout()
   {
-    this.auth.signOut();
+    const alert = await this.alertController.create({
+      header: 'Se déconnecter de Greenium!',
+      message: 'Voulez-vous vraiment <strong>vous déconnecter ?</strong>',
+      buttons: [
+      {
+      text: 'Annuler',
+      role: 'cancel',
+      cssClass: 'secondary',
+      handler: (blah) => {
+        console.log('Confirm Cancel: blah');
+      }
+    }, {
+      text: 'Me déconnecter',
+      handler: () => {this.auth.signOut();
+        console.log('Confirm Okay');
+      }
+    }
+  ]
+  });
+  
+  await alert.present();
+  let result = await alert.onDidDismiss();
+  console.log(result);
   }
   gotoProfile()
   {
@@ -31,4 +57,5 @@ export class AdamPage {
   guide(){
     this.router.navigate(['/guide']);
 }
+
 }
